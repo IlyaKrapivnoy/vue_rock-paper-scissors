37c7d546a330eba0b6278c68de08786036153152
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-28 12:48:59 +0100

    add icons and set outcomes

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,31 @@
 <script setup>
+import { ref, computed, onMounted } from "vue";
+
+const wins = ref(0);
+const draws = ref(0);
+const losses = ref(0);
+
+const choices = ref(null);
+const computerChoice = ref(null);
+const verdict = ref(null);
+
+const outcomes = {
+  rock: {
+    rock: "draw",
+    paper: "loss",
+    scissors: "win",
+  },
+  paper: {
+    rock: "win",
+    paper: "draw",
+    scissors: "loss",
+  },
+  scissors: {
+    rock: "loss",
+    paper: "win",
+    scissors: "draw",
+  },
+};
 </script>
 
 <template>
